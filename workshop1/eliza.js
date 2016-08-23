16f96fd52c57b295cfc52be5bd5b6a5588c1fb2c
AlexisGebbia <ag8336@bard.edu>
2016-08-23 11:53:36 -0400

    Update eliza.js

--- workshop1/eliza.js
+++ workshop1/eliza.js
@@ -28,204 +28,96 @@
  */
 
 var rules = {
-  "~*x hello ~*y": [
-    "How do you do. Please state your problem.'
-    "i was just born',
-  ],
-  "~*x computer ~*y": [
-    "Do computers worry you?",
-    "What do you think about machines have to do with your problem?",
-    "Don't you think computers can help people",
-    "Why do you mention computers?",
-    "What about computers worries you?",
-    "What do you think about computers?",
-    "What do you think machines have to do with your problem?",
-  ],
-  "~*x name ~*y": [
-    "I am not interested in names",
-    "my namw is cookie"
-  ],
-  "~*x sorry ~*y": [
-    "Please don't apologize",
-    "Apologies are not necessary",
-    "What feelings do you have when you apologize",
-  ],
   "~*x I remember ~*y": [
-    "Do you often think of ~y?",
-    "Does thinking of ~y bring anything else to mind?",
-    "What else do you remember?",
-    "Why do you recall ~y right now?",
-    "What in the present situation reminds you of ~y?",
-    "What is the connection between me and ~y?",
+    "was the cause was the remedy was the record was the argument",
   ],
+ 
   "~*x do you remember ~*y": [
-    "Did you think I would forget ~y?",
-    "Why do you think I should recall ~y now?",
-    "What about ~y?",
-    "You mentioned ~y",
+    "was the delay was the evidence was overboard was the not was the",
   ],
   "~*x I want ~*y": [
-    "What would it mean if you got ~y?",
-    "Why do you want ~y?",
-    "Suppose you got ~y soon.",
+    "cause was the was was the need was the case was the perils was the",
   ],
     "~*x I need ~*y": [
-    "What would it mean if you got ~y?",
-    "Why do you need ~y?",
-    "Suppose you got ~y soon.",
+    "want was the particular circumstanec was the seas was the costs",
   ],
   "~*x if ~*y": [
-    "Do you really think it's likely that ~y?",
-    "Do you wish that ~y?",
-    "What do you think about ~y?",
-    "Really--if ~y?",
+    "the seas the rains was uncommon was the declaration",
   ],
   "~*x I dreamt ~*y": [
-    "Really-- ~y",
-    "Have you ever fantasized ~y while you were awake",
-    "Have you dreamt of ~y before?",
+    "The wilderness as metaphor is in theis case not evocative enough because causing a complete failure in the magnet, the compass, the scale, the stars and the movement of the rivers is more than getting lost in the woods.",
   ],
   "~*x I dream about ~*y": [
-    "How do you feel about ~y in reality?",
+    "The sentence which insist on tenses and words like later and before",
   ],
   "~*x dream ~*y": [
-    "What does this dream suggest to you?",
-    "Do you dream often?",
-    "What persons appear in your dreams?",
-    "Don't you believe that dream has to do with your problem?",
+    "I have been here before, I will return",
   ],
   "~*x my mother ~*y": [
-    "Who else in your family ~y?",
-    "Tell me more about your family",
+    "No one in the world seems to know exactly what it describes, not even we motley millions who call ourselves Americans.",
   ],
   "~*x my father ~*y": [
-    "Your father?",
-    "Does he influence you strongly?",
-    "What else comes to mind when you think of your father?",
+    "It seemed incredible that this day, a day without warnings or omens, might be that of my implacable death.",
   ],
   "~*x I am glad ~*y": [
-    "How have I helped you to be ~y?",
-    "What makes you happy just now?",
-    "Can you explain why you are suddenly ~y?",
+    "Then I reflected that all things happen, happen to one, precisely now.",
   ],
   "~*x I am sad ~*y": [
-    "I am sorry to hear you are depressed",
-    "I'm sure it's not pleasant to be sad",
+    "After all, the point of art is to show people that life is worth living by showing that it isn't",
   ],
   "~*x are like ~*y": [
-    "What resemblence do you see between ~x and ~y?",
+    "the captain the crew the authorize",
   ],
   "~*x is like ~*y": [
-    "In what way is it that ~x is like ~y?",
-    "What resemblence do you see?",
-    "Could there really be some connection?",
-    "How?",
+    "in captain crew & could",
   ],
   "~*x alike ~*y": [
-    "In what way?",
-    "What similarities are there?",
+    "could or justify authorize",
   ],
   "~*x same ~*y": [
-    "What other connections do you see?",
+    "Compassion is an unstable emotion. It needs to be translated into action, or it withers.",
   ],
   "~*x no ~*y": [
-    "Why not?",
-    "You are being a bit negative.",
-    "Are you saying 'No' just to be negative?",
+    "I'm full of outer space",
   ],
   "~*x I was ~*y": [
-    "Were you really?",
-    "Perhaps I already knew you were ~y.",
-    "Why do you tell me you were ~y now?",
+    "I'm free as fred all night.",
   ],
   "~*x was I ~*y": [
-    "What if you were ~y?",
-    "Do you think you were ~y?",
-    "What would it mean if you were ~y?",
+    "Some disasters are more apt subjects of irony than others?",
   ],
   "~*x I am ~*y": [
-    "In what way are you ~y?",
-    "Do you want to be ~y?",
+    "Our sympathy proclaims our innocence as well as our impotence",
   ],
   "~*x am I ~*y": [
-    "Do you believe you are ~y?",
-    "Would you want to be ~y?",
-    "You wish I would tell you you are ~y?",
-    "What would it mean if you were ~y?",
+    "A lot of it is just trying to figure out how to say something",
   ],
   "~*x am ~*y": [
-    "Why do you say 'AM?'",
-    "I don't understand that",
+    "The states described as apathy, moral or emotional anesthesis, are full of feelings; the feelings are rage and frustration",
   ],
   "~x are ~*y": [
-    "Did you think they might not be ~y?",
-    "Possibly they are ~y",
+    "I dont wanna represent anything and I don't want to repair anything.",
   ],
    "~*x are you ~*y": [
-    "Why are you interested in whether I am ~y or not?",
-    "Would you prefer if I weren't ~y ?",
-    "Perhaps I am ~y in your fantasies.",
+    "Babies making grammarless babies",
   ],
   "~*x you are ~*y": [
-    "What makes you think I am ~y?",
+    "(you caught me) smilin' ",
   ],
   "~*x because ~*y": [
-    "Is that the real reason?",
-    "What other reasons might there be?",
-    "Does that reason seem to explain anything else?",
+    "Heaven. It never goes astray.",
   ],
   "~*x were you ~*y": [
-    "Perhaps I was ~y?",
-    "What do you think?",
-    "What if I had been ~y?",
+    "To acknowledge the existence of the incorrigible",
   ],
   "~*x I can't ~*y": [
-    "Maybe you could ~y now",
-    "What if you could ~y?",
+    "She was the site of the struggle itself.",
   ],
   "~*x I feel ~*y": [
-    "Do you often feel ~y?"
+    "Peopel can turn off not just because a steady diet of images of vilonece has made them indifferent but because they are afraid."
   ],
   "~*x I felt ~*y": [
-    "What other feelings do you have?"
-  ],
-  "~*x I ~*y you ~*z": [
-    "Perhaps in your fantasy we ~y each other",
-  ],
-  "~*x why don't you ~*y": [
-    "Should you ~y yourself?",
-    "Do you believe I don't ~y ?",
-    "Perhaps I will ~y in good time",
-  ],
-  "~*x yes ~*y": [
-    "You seem quite positive",
-    "You are sure?",
-    "I understand",
-  ],
-  "~*x someone ~*y": [
-    "Can you be more specific?",
-  ],
-  "~*x everyone ~*y": [
-    "Surely not everyone",
-    "Can you think of anyone in particular?",
-    "Who, for example?",
-    "You are thinking of a special person",
-  ],
-  "~*x always ~*y": [
-    "Can you think of a specific example?",
-    "When?",
-    "What incident are you thinking of?",
-    "Really--always?",
-  ],
-  "~*x what ~*y": [
-    "Why do you ask?",
-    "Does that question interest you?",
-    "What is it you really want to know?",
-    "What do you think?",
-    "What comes to your mind when you ask that?",
-  ],
-  "~*x perhaps ~*y": [
-    "You do not seem quite certain",
+    "the means to achieve the end would already be the end; and this 'end,' conversely, cannot be considered a means in some other respect, because there is nothing higher to attain than this actuality itself."
   ],
 };
 
